fbc93650a390becf2f5f4ee66128d0b3257c5175
titan2903 <titanioy98@gmail.com>
2020-04-01 18:05:24 +0700

    update add cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
 const Todos = require('./router');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 
 app.use(Todos)
